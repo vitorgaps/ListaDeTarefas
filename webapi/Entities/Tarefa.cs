9239ec5bef009d111c79d94df0aa974cccc0aa8b
Vitor <vitorgabriel616@gmail.com>
2023-03-26 18:51:07 -0300

    Database connection implemented - Postgresql connector added - Tarefa entity created

---
+++ webapi/Entities/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace webapi.Entities
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string NomeTarefa { get; set; } = "";
+        public string? Descricao { get; set; } = "";
+        public DateTime DataInicio { get; set; }
+        public DateTime? DataTermino { get; set; }        
+        public bool Terminado { get; set; }
+    }
+}
---
+++ webapi/Helpers/DataContext.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using webapi.Entities;
+
+namespace webapi.Helpers;
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarefa>()
+            .Property(m=>m.Id)
+            .ValueGeneratedOnAdd();
+        modelBuilder.Entity<Tarefa>()
+            .Property(m => m.NomeTarefa).IsRequired();
+        modelBuilder.Entity<Tarefa>()
+            .Property(m => m.DataInicio).IsRequired();
+        modelBuilder.Entity<Tarefa>()
+            .Property(m => m.Terminado).IsRequired();
+            
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<Tarefa> Tarefas { get; set; }
+}
